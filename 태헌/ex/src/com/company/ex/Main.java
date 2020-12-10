0ca530880ea467c4dc81ff281309a4c8e09d9ebe
HOSIHO <kksan33@naver.com>
2020-12-10 22:08:04 +0900

    2주차 수업 정리

---
+++ 태헌/ex/src/com/company/ex/Main.java
@@ -0,0 +1,11 @@
+package com.company.ex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("난김태헌");
+        sdf sdf =new sdf(1);
+
+
+    }
+}
---
+++ 태헌/ex/src/com/company/ex_sdf/SDF.java
@@ -0,0 +1,7 @@
+package com.company.ex_sdf;
+
+public class SDF {
+    final int a;
+    int b;
+    public
+}
